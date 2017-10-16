512ed32c48b73da22cc495f98c75a9e4e27824c2
funkeydiscoman <jacksonrfrank@gmail.com>
2017-10-16 10:40:33 -0700

    Activity 1 Done

--- src/CardTester.java
+++ src/CardTester.java
@@ -8,6 +8,17 @@ public class CardTester {
 	 *	@param args is not used.
 	 */
 	public static void main(String[] args) {
-		/* *** TO BE IMPLEMENTED IN ACTIVITY 1 *** */
+		Card c1 = new Card("K", "Spades", 10);
+		Card c2 = new Card("3", "Clubs", 3);
+		Card c3 = new Card("K", "Spades", 10);
+		
+		System.out.println(c1.toString());
+		System.out.println(c2.toString());
+		System.out.println(c3.toString());
+		System.out.println();
+		
+		System.out.println(c1.matches(c2));
+		System.out.println(c1.matches(c3));
+		System.out.println(c2.matches(c3));
 	}
 }
